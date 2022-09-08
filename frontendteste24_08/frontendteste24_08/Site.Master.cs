e14c58d72ad74b6c3034f60733860c8b909b4001
anajuliasouza08 <111933598+anajuliasouza08@users.noreply.github.com>
2022-09-08 19:09:12 -0300

    pulling from dev (#31)
    * FrontEnd
    
    * FrontEnd Pagina 1
    
    * Mer e modelo lógico
    
    * FrontEnd
    
    * Fix
    
    * FrontEnd
    
    * fix
    
    * Igor (#21)
    
    * front conexão
    
    * front conexão
    
    Co-authored-by: Marotti <castilhogabriel430@gmail.com>
    
    * Igor (#21) (#22)
    
    Co-authored-by: Marotti <castilhogabriel430@gmail.com>
    Co-authored-by: AnaaGabriela <107006566+AnaaGabriela@users.noreply.github.com>
    Co-authored-by
    
    * FrontEndDropDownList
    
    * MER e ML
    
    * FrontFixado
    
    * fix
    
    * fix branch
    
    * fix branching dev -> main (#28) (#29)
    
    * FrontEnd
    
    * FrontEnd Pagina 1
    
    * Mer e modelo lógico
    
    * FrontEnd
    
    * Fix
    
    * FrontEnd
    
    * Igor (#21)
    
    * front conexão
    
    * front conexão
    
    Co-authored-by: Marotti <castilhogabriel430@gmail.com>
    
    * FrontEndDropDownList
    
    * MER e ML
    
    * FrontFixado
    
    * fix
    
    * fix branch
    
    Co-authored-by: Igor Vital <igorv1290@gmail.com>
    Co-authored-by: Marotti <castilhogabriel430@gmail.com>
    Co-authored-by: AnaaGabriela <107006566+AnaaGabriela@users.noreply.github.com>
    Co-authored-by: RenanOliveiraa <107006438+RenanOliveiraa@users.noreply.github.com>
    
    Co-authored-by: Igor Vital <igorv1290@gmail.com>
    Co-authored-by: Marotti <castilhogabriel430@gmail.com>
    Co-authored-by: AnaaGabriela <107006566+AnaaGabriela@users.noreply.github.com>
    Co-authored-by: RenanOliveiraa <107006438+RenanOliveiraa@users.noreply.github.com>
    
    * Anagabriela (#30)
    
    * SelectPlacaMãe
    
    * Select Processador Fonte SSD
    
    * Todos os Selects
    
    Co-authored-by: Igor Vital <igorv1290@gmail.com>
    Co-authored-by: Marotti <castilhogabriel430@gmail.com>
    Co-authored-by: anajuliasouza <anajuliasouza02.ajs@gmail.com>
    Co-authored-by: AnaaGabriela <107006566+AnaaGabriela@users.noreply.github.com>
    Co-authored-by: Lucas Grossi <101760538+LgrossiC@users.noreply.github.com>
    Co-authored-by: RenanOliveiraa <107006438+RenanOliveiraa@users.noreply.github.com>

--- frontendteste24_08/frontendteste24_08/Site.Master.cs
+++ frontendteste24_08/frontendteste24_08/Site.Master.cs
@@ -9,9 +9,18 @@ namespace frontendteste24_08
 {
     public partial class SiteMaster : MasterPage
     {
+        public static string ConnectionString = "Server=127.0.0.1;User ID=root;Password=;Database=loja_pc";
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+        public static void ExibirAlert(Page page, string mensagem)
+        {
+            page.ClientScript.RegisterStartupScript(
+                 page.GetType(),
+                 "MessageBox" + Guid.NewGuid(),
+                 "<script language='javascript'>alert('" + mensagem + "');</script>"
+              );
         }
     }
 }
