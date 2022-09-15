57443c3261edd083d798b8fb60663d3f2d64e050
LucasRyanAugusto <Lucasryanaugusto@gmail.com>
2022-09-14 22:10:50 -0300

    Página do Admin

--- PROJETO/frontendteste24_08/Admin.aspx.cs
+++ PROJETO/frontendteste24_08/Admin.aspx.cs
@@ -12,23 +12,39 @@ namespace frontendteste24_08
     public partial class Admin : System.Web.UI.Page
     {
         private MySqlConnection connection;
-        protected void Page_Load(object sender, EventArgs e) //Quando a página carrega.
-        {                                  
-            connection.Open();
 
-            connection = new MySqlConnection(SiteMaster.ConnectionString);     
+        protected void Page_Load(object sender, EventArgs e) //Quando a página carrega.
+        {
+            if (!IsPostBack)
+            {
+            }
+
+            connection = new MySqlConnection(SiteMaster.ConnectionString);    
+                         
             DataTable tabela = new DataTable();
 
             tabela.Columns.Add("descricao");
             tabela.Columns.Add("tipo");
+            tabela.Columns.Add("comprar");
+            tabela.Columns.Add("gastos");
 
-            var commando = new MySqlCommand($"SELECT descricao, tipo FROM componentes", connection);
+            connection.Open();
+            double total_geral = 0;
+            var commando = new MySqlCommand($"SELECT descrição, tipo, estoque, valor FROM componentes", connection);
             var reader = commando.ExecuteReader();
             while (reader.Read())
-            {
+            {             
+                int estoque = reader.GetInt32("estoque");
+                double valor_c = reader.GetDouble("valor");             
                 var linha = tabela.NewRow();
-                linha["descricao"] = reader.GetString("descricao");
+                linha["descricao"] = reader.GetString("descrição");
                 linha["tipo"] = reader.GetString("tipo");
+                int comprar = 1000 - estoque;
+                double valor_t = valor_c * comprar;
+                total_geral = total_geral + valor_t;
+                linha["comprar"] = comprar;
+                linha["gastos"] = valor_t.ToString("C");
+
 
                 tabela.Rows.Add(linha);
             }
@@ -36,9 +52,9 @@ namespace frontendteste24_08
             grdComponentes.DataSource = tabela;
             grdComponentes.DataBind();
 
-
+            
             connection.Close();
-
+            valor_total.Text = Convert.ToString(total_geral.ToString("C"));
         }
     }
-    }
+}
--- PROJETO/frontendteste24_08/Admin.aspx.designer.cs
+++ PROJETO/frontendteste24_08/Admin.aspx.designer.cs
@@ -20,5 +20,14 @@ namespace frontendteste24_08 {
         /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
         /// </remarks>
         protected global::System.Web.UI.WebControls.GridView grdComponentes;
+        
+        /// <summary>
+        /// Controle valor_total.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox valor_total;
     }
 }
